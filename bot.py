67cdbc7861a9e91a8ce94423fb064699cef70dd1
Kaishuto <32487322+Kaishuto@users.noreply.github.com>
2017-10-04 08:04:45 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -154,44 +154,44 @@ def SEND_MESSAGE(op):
 				 contact = client.getContact(key)
 				 sendMessage(msg.to, ""+contact.displayName+" I invited you")
 	#-------------------------------------------------------------
-	 if msg.text == "set":
-                    sendMessage(msg.to, "I have set a read point ♪\n「tes」I will show you who I have read ♪")
-                    try:
-                        del wait['readPoint'][msg.to]
-                        del wait['readMember'][msg.to]
-                    except:
-                        pass
-                    wait['readPoint'][msg.to] = msg.id
-                    wait['readMember'][msg.to] = ""
-                    wait['setTime'][msg.to] = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
-                    wait['ROM'][msg.to] = {}
-                    print wait
-                if msg.text == "tes":
-                    if msg.to in wait['readPoint']:
-                        if wait["ROM"][msg.to].items() == []:
-                            chiya = ""
-                        else:
-                            chiya = ""
-                            for rom in wait["ROM"][msg.to].items():
-                                print rom
-                                chiya += rom[1] + "\n"
+		 if msg.text == "set":
+						sendMessage(msg.to, "I have set a read point ♪\n「tes」I will show you who I have read ♪")
+						try:
+							del wait['readPoint'][msg.to]
+							del wait['readMember'][msg.to]
+						except:
+							pass
+						wait['readPoint'][msg.to] = msg.id
+						wait['readMember'][msg.to] = ""
+						wait['setTime'][msg.to] = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+						wait['ROM'][msg.to] = {}
+						print wait
+					if msg.text == "tes":
+						if msg.to in wait['readPoint']:
+							if wait["ROM"][msg.to].items() == []:
+								chiya = ""
+							else:
+								chiya = ""
+								for rom in wait["ROM"][msg.to].items():
+									print rom
+									chiya += rom[1] + "\n"
 
-                        sendMessage(msg.to, "People who readed %s\nthat's it\n\nPeople who have ignored reads\n%sIt is abnormal ♪\n\nReading point creation date n time:\n[%s]"  % (wait['readMember'][msg.to],chiya,setTime[msg.to]))
-                    else:
-                        sendMessage(msg.to, "An already read point has not been set.\n「set」you can send ♪ read point will be created ♪")
-                else:
-                    pass
-        else:
-            pass
+							sendMessage(msg.to, "People who readed %s\nthat's it\n\nPeople who have ignored reads\n%sIt is abnormal ♪\n\nReading point creation date n time:\n[%s]"  % (wait['readMember'][msg.to],chiya,setTime[msg.to]))
+						else:
+							sendMessage(msg.to, "An already read point has not been set.\n「set」you can send ♪ read point will be created ♪")
+					else:
+						pass
+			else:
+				pass
 	#-------------------------------------------------------------
-	if msg.text == "cancel":
-            	        group = client.getGroup(msg.to)
-            	        if group.invitee is None:
-              	          sendMessage(op.message.to, "No one is inviting.")
-              	      else:
-               	         gInviMids = [contact.mid for contact in group.invitee]
-               	         client.cancelGroupInvitation(msg.to, gInviMids)
-               	         sendMessage(msg.to, str(len(group.invitee)) + " Done")
+		if msg.text == "cancel":
+							group = client.getGroup(msg.to)
+							if group.invitee is None:
+							  sendMessage(op.message.to, "No one is inviting.")
+						  else:
+							 gInviMids = [contact.mid for contact in group.invitee]
+							 client.cancelGroupInvitation(msg.to, gInviMids)
+							 sendMessage(msg.to, str(len(group.invitee)) + " Done")
 	#-------------------------------------------------------------
 					if msg.text == "Rx-D5N":
 						sendMessage(msg.to, text="gift sent", contentMetadata=None, contentType=9)
