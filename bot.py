0be4e4bac483f310a923902a02153630a2e7b074
Kaishuto <32487322+Kaishuto@users.noreply.github.com>
2017-10-04 07:44:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,199 +1,199 @@
-# -*- coding: utf-8 -*-
-# Edited from script LineVodka script made by Rx-D5N
-from LineAlpha import LineClient
-from LineAlpha.LineApi import LineTracer
-from LineAlpha.LineThrift.ttypes import Message
-from LineAlpha.LineThrift.TalkService import Client
-import time, datetime, random ,sys, re, string, os, json
+	# -*- coding: utf-8 -*-
+	# Edited from script LineVodka script made by Rx-D5N
+	from LineAlpha import LineClient
+	from LineAlpha.LineApi import LineTracer
+	from LineAlpha.LineThrift.ttypes import Message
+	from LineAlpha.LineThrift.TalkService import Client
+	import time, datetime, random ,sys, re, string, os, json
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
 
-client = LineClient()
-client._qrLogin("line://au/q/")
+	client = LineClient()
+	client._qrLogin("line://au/q/")
 
-profile, setting, tracer = client.getProfile(), client.getSettings(), LineTracer(client)
-offbot, messageReq, wordsArray, waitingAnswer = [], {}, {}, {}
+	profile, setting, tracer = client.getProfile(), client.getSettings(), LineTracer(client)
+	offbot, messageReq, wordsArray, waitingAnswer = [], {}, {}, {}
 
-print client._loginresult()
+	print client._loginresult()
 
-wait = {
-    'readPoint':{},
-    'readMember':{},
-    'setTime':{},
-    'ROM':{}
-   }
+	wait = {
+		'readPoint':{},
+		'readMember':{},
+		'setTime':{},
+		'ROM':{}
+	   }
 
-setTime = {}
-setTime = wait["setTime"]
+	setTime = {}
+	setTime = wait["setTime"]
 
-def sendMessage(to, text, contentMetadata={}, contentType=0):
-    mes = Message()
-    mes.to, mes.from_ = to, profile.mid
-    mes.text = text
+	def sendMessage(to, text, contentMetadata={}, contentType=0):
+		mes = Message()
+		mes.to, mes.from_ = to, profile.mid
+		mes.text = text
 
-    mes.contentType, mes.contentMetadata = contentType, contentMetadata
-    if to not in messageReq:
-        messageReq[to] = -1
-    messageReq[to] += 1
-    client._client.sendMessage(messageReq[to], mes)
+		mes.contentType, mes.contentMetadata = contentType, contentMetadata
+		if to not in messageReq:
+			messageReq[to] = -1
+		messageReq[to] += 1
+		client._client.sendMessage(messageReq[to], mes)
 
-def NOTIFIED_ADD_CONTACT(op):
-    try:
-        sendMessage(op.param1, client.getContact(op.param1).displayName + "Thanks for add")
-    except Exception as e:
-        print e
-        print ("\n\nNOTIFIED_ADD_CONTACT\n\n")
-        return
+	def NOTIFIED_ADD_CONTACT(op):
+		try:
+			sendMessage(op.param1, client.getContact(op.param1).displayName + "Thanks for add")
+		except Exception as e:
+			print e
+			print ("\n\nNOTIFIED_ADD_CONTACT\n\n")
+			return
 
-tracer.addOpInterrupt(5,NOTIFIED_ADD_CONTACT)
+	tracer.addOpInterrupt(5,NOTIFIED_ADD_CONTACT)
 
-def NOTIFIED_ACCEPT_GROUP_INVITATION(op):
-    #print op
-    try:
-        sendMessage(op.param1, client.getContact(op.param2).displayName + "WELCOME to " + group.name)
-    except Exception as e:
-        print e
-        print ("\n\nNOTIFIED_ACCEPT_GROUP_INVITATION\n\n")
-        return
+	def NOTIFIED_ACCEPT_GROUP_INVITATION(op):
+		#print op
+		try:
+			sendMessage(op.param1, client.getContact(op.param2).displayName + "WELCOME to " + group.name)
+		except Exception as e:
+			print e
+			print ("\n\nNOTIFIED_ACCEPT_GROUP_INVITATION\n\n")
+			return
 
-tracer.addOpInterrupt(17,NOTIFIED_ACCEPT_GROUP_INVITATION)
+	tracer.addOpInterrupt(17,NOTIFIED_ACCEPT_GROUP_INVITATION)
 
-def NOTIFIED_KICKOUT_FROM_GROUP(op):
-    try:
-        sendMessage(op.param1, client.getContact(op.param3).displayName + " Good Bye\n(*´･ω･*)")
-    except Exception as e:
-        print e
-        print ("\n\nNOTIFIED_KICKOUT_FROM_GROUP\n\n")
-        return
+	def NOTIFIED_KICKOUT_FROM_GROUP(op):
+		try:
+			sendMessage(op.param1, client.getContact(op.param3).displayName + " Good Bye\n(*´･ω･*)")
+		except Exception as e:
+			print e
+			print ("\n\nNOTIFIED_KICKOUT_FROM_GROUP\n\n")
+			return
 
-tracer.addOpInterrupt(19,NOTIFIED_KICKOUT_FROM_GROUP)
+	tracer.addOpInterrupt(19,NOTIFIED_KICKOUT_FROM_GROUP)
 
-def NOTIFIED_LEAVE_GROUP(op):
-    try:
-        sendMessage(op.param1, client.getContact(op.param2).displayName + " Good Bye\n(*´･ω･*)")
-    except Exception as e:
-        print e
-        print ("\n\nNOTIFIED_LEAVE_GROUP\n\n")
-        return
+	def NOTIFIED_LEAVE_GROUP(op):
+		try:
+			sendMessage(op.param1, client.getContact(op.param2).displayName + " Good Bye\n(*´･ω･*)")
+		except Exception as e:
+			print e
+			print ("\n\nNOTIFIED_LEAVE_GROUP\n\n")
+			return
 
-tracer.addOpInterrupt(15,NOTIFIED_LEAVE_GROUP)
+	tracer.addOpInterrupt(15,NOTIFIED_LEAVE_GROUP)
 
-def NOTIFIED_READ_MESSAGE(op):
-    #print op
-    try:
-        if op.param1 in wait['readPoint']:
-            Name = client.getContact(op.param2).displayName
-            if Name in wait['readMember'][op.param1]:
-                pass
-            else:
-                wait['readMember'][op.param1] += "\n・" + Name
-                wait['ROM'][op.param1][op.param2] = "・" + Name
-        else:
-            pass
-    except:
-        pass
+	def NOTIFIED_READ_MESSAGE(op):
+		#print op
+		try:
+			if op.param1 in wait['readPoint']:
+				Name = client.getContact(op.param2).displayName
+				if Name in wait['readMember'][op.param1]:
+					pass
+				else:
+					wait['readMember'][op.param1] += "\n・" + Name
+					wait['ROM'][op.param1][op.param2] = "・" + Name
+			else:
+				pass
+		except:
+			pass
 
-tracer.addOpInterrupt(55, NOTIFIED_READ_MESSAGE)
+	tracer.addOpInterrupt(55, NOTIFIED_READ_MESSAGE)
 
-def RECEIVE_MESSAGE(op):
-    msg = op.message
-    try:
-        if msg.contentType == 0:
-            try:
-                if msg.to in wait['readPoint']:
-                    if msg.from_ in wait["ROM"][msg.to]:
-                        del wait["ROM"][msg.to][msg.from_]
-                else:
-                    pass
-            except:
-                pass
-        else:
-            pass
-    except KeyboardInterrupt:
-	       sys.exit(0)
-    except Exception as error:
-        print error
-        print ("\n\nRECEIVE_MESSAGE\n\n")
-        return
+	def RECEIVE_MESSAGE(op):
+		msg = op.message
+		try:
+			if msg.contentType == 0:
+				try:
+					if msg.to in wait['readPoint']:
+						if msg.from_ in wait["ROM"][msg.to]:
+							del wait["ROM"][msg.to][msg.from_]
+					else:
+						pass
+				except:
+					pass
+			else:
+				pass
+		except KeyboardInterrupt:
+			   sys.exit(0)
+		except Exception as error:
+			print error
+			print ("\n\nRECEIVE_MESSAGE\n\n")
+			return
 
-tracer.addOpInterrupt(26, RECEIVE_MESSAGE)
+	tracer.addOpInterrupt(26, RECEIVE_MESSAGE)
 
-def SEND_MESSAGE(op):
-    msg = op.message
-    try:
-        if msg.toType == 2:
-            if msg.contentType == 0:
-                #if "gname:" in msg.text:
-#--------------------------------------------------------------
-	if msg.text == "play":
-		print "ok"
-			_name = msg.text.replace("Start","")
-			gs = client.getGroup(msg.to)
-			sendMessage(msg.to,"Why So Serious?")
-			targets = []
-			for g in gs.members:
-                        if _name in g.displayName:
-                            targets.append(g.mid)
-                    if targets == []:
-                        sendMessage(msg.to,"error")
-                    else:
-                        for target in targets:
-                            try:
-                                klist=[client]
-                                kicker=random.choice(klist)
-                                kicker.kickoutFromGroup(msg.to,[target])
-                                print (msg.to,[g.mid])
-                            except:
-                                sendText(msg.to,"error")
-#-------------------------------------------------------------
- 		if "invite:" in msg.text:
-			 key = msg.text[-33:]
-			 client.findAndAddContactsByMid(key)
-			 client.inviteIntoGroup(msg.to, [key])
-			 contact = client.getContact(key)
-			 sendMessage(msg.to, ""+contact.displayName+" I invited you")
-#-------------------------------------------------------------
- 		if msg.text == "set":
-		 sendMessage(msg.to, "I have set a read point ♪\n「tes」I will show you who I have read ♪")
- 	try:
- 		del wait['readPoint'][msg.to]
- 		del wait['readMember'][msg.to]
- 	except:
- pass
- wait['readPoint'][msg.to] = msg.id
- wait['readMember'][msg.to] = ""
- wait['setTime'][msg.to] = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
- wait['ROM'][msg.to] = {}
- print wait
- if msg.text == "tes":
- if msg.to in wait['readPoint']:
- if wait["ROM"][msg.to].items() == []:
- chiya = ""
- else:
- chiya = ""
- for rom in wait["ROM"][msg.to].items():
- print rom
- chiya += rom[1] + "\n"
- sendMessage(msg.to, "People who readed %s\nthat's it\n\nPeople who have ignored reads\n%sIt is abnormal ♪\n\nReading point creation date n time:\n[%s]"  % (wait['readMember'][msg.to],chiya,setTime[msg.to]))
- else:
- sendMessage(msg.to, "An already read point has not been set.\n「set」you can send ♪ read point will be created ♪")
- else:
- pass
- else:
- pass
-#-------------------------------------------------------------
-                if msg.text == "Rx-D5N":
-                    sendMessage(msg.to, text="gift sent", contentMetadata=None, contentType=9)
-        else:
-            pass
+	def SEND_MESSAGE(op):
+		msg = op.message
+		try:
+			if msg.toType == 2:
+				if msg.contentType == 0:
+					#if "gname:" in msg.text:
+	#--------------------------------------------------------------
+		if msg.text == "play":
+			print "ok"
+				_name = msg.text.replace("Start","")
+				gs = client.getGroup(msg.to)
+				sendMessage(msg.to,"Why So Serious?")
+				targets = []
+				for g in gs.members:
+							if _name in g.displayName:
+								targets.append(g.mid)
+						if targets == []:
+							sendMessage(msg.to,"error")
+						else:
+							for target in targets:
+								try:
+									klist=[client]
+									kicker=random.choice(klist)
+									kicker.kickoutFromGroup(msg.to,[target])
+									print (msg.to,[g.mid])
+								except:
+									sendText(msg.to,"error")
+	#-------------------------------------------------------------
+			if "invite:" in msg.text:
+				 key = msg.text[-33:]
+				 client.findAndAddContactsByMid(key)
+				 client.inviteIntoGroup(msg.to, [key])
+				 contact = client.getContact(key)
+				 sendMessage(msg.to, ""+contact.displayName+" I invited you")
+	#-------------------------------------------------------------
+			if msg.text == "set":
+			 sendMessage(msg.to, "I have set a read point ♪\n「tes」I will show you who I have read ♪")
+		try:
+			del wait['readPoint'][msg.to]
+			del wait['readMember'][msg.to]
+		except:
+	 pass
+	 wait['readPoint'][msg.to] = msg.id
+	 wait['readMember'][msg.to] = ""
+	 wait['setTime'][msg.to] = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+	 wait['ROM'][msg.to] = {}
+	 print wait
+	 if msg.text == "tes":
+	 if msg.to in wait['readPoint']:
+	 if wait["ROM"][msg.to].items() == []:
+	 chiya = ""
+	 else:
+	 chiya = ""
+	 for rom in wait["ROM"][msg.to].items():
+	 print rom
+	 chiya += rom[1] + "\n"
+	 sendMessage(msg.to, "People who readed %s\nthat's it\n\nPeople who have ignored reads\n%sIt is abnormal ♪\n\nReading point creation date n time:\n[%s]"  % (wait['readMember'][msg.to],chiya,setTime[msg.to]))
+	 else:
+	 sendMessage(msg.to, "An already read point has not been set.\n「set」you can send ♪ read point will be created ♪")
+	 else:
+	 pass
+	 else:
+	 pass
+	#-------------------------------------------------------------
+					if msg.text == "Rx-D5N":
+						sendMessage(msg.to, text="gift sent", contentMetadata=None, contentType=9)
+			else:
+				pass
 
-    except Exception as e:
-        print e
-        print ("\n\nSEND_MESSAGE\n\n")
-        return
+		except Exception as e:
+			print e
+			print ("\n\nSEND_MESSAGE\n\n")
+			return
 
-tracer.addOpInterrupt(25,SEND_MESSAGE)
+	tracer.addOpInterrupt(25,SEND_MESSAGE)
 
-while True:
-    tracer.execute()
+	while True:
+		tracer.execute()
